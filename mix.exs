ecce33b2050814ee01aa00326faca33589390116
Victor Azevedo <victorazesc@hotmail.com>
2024-05-08 09:39:10 -0300

    feat: password fucntion

--- mix.exs
+++ mix.exs
@@ -53,7 +53,9 @@ defmodule MediumGraphqlApi.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:comeonin, "~> 4.0"},
+      {:argon2_elixir, "~> 1.2"}
     ]
   end
 
